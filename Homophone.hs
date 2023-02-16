5dfc2fb2f163ef69aae63c3887b86ba157bedfd2
Nurdayana Muhd Faisal <dam20@imperial.ac.uk>
2023-02-16 21:54:45 +0000

    created arpabet data type

--- Homophone.hs
+++ Homophone.hs
@@ -6,7 +6,14 @@ import qualified Data.Map as Map
 import Data.IntMap (fromList)
 import Data.Char
 
-import MergedDict
+-- import MergedDict
+
+data ARPABET = AA | AE | AH | AO | AW | AX | AXR | AY | EH | ER | EY | -- VOWELS
+               IH | IX | IY | OW | OY | UH | UW | UX |                 -- VOWELS
+               B | CH | D | DH | DX | EL | EM | EN | F | G | HH | JH | -- CONSONENTS
+               K | L | M | N | NG | NX | P | Q | R | S | SH | T | TH | -- CONSONENTS
+               V | W | WH | Y | Z | ZH                                 -- CONSONENTS
+
 
 testList :: [(String, [String])]
 testList = [("A",["AH0","EY1"]),
@@ -15,16 +22,18 @@ testList = [("A",["AH0","EY1"]),
     ("A.'S",["EY1 Z"]),
     ("A.D.",["EY2 D IY1"]),
     ("A.M.",["EY2 EH1 M"]),
+    ("THEIR", ["DH EH1 R"]), 
+    ("THERE", ["DH EH1 R"]), 
     ("PATRON",["P EY1 T R AH0 N"]),
+    ("RED", ["R EH1 D"]), 
+    ("REED",["R IY1 D"]),
+    ("READ",["R EH1 D","R IY1 D"]),
     ("PATRONAGE",["P AE1 T R AH0 N IH0 JH","P EY1 T R AH0 N AH0 JH","P EY1 T R AH0 N IH0 JH"]),
-    ("PATRONE",["P AA0 T R OW1 N IY0"]),
-    ("PATRONESS",["P EY1 T R AH0 N AH0 S"]),
-    ("PATRONIZE",["P EY1 T R AH0 N AY2 Z","P AE1 T R AH0 N AY2 Z"]),
-    ("PATRONIZED",["P EY1 T R AH0 N AY2 Z D","P AE1 T R AH0 N AY2 Z D"]),
     ("PATRONIZES",["P EY1 T R AH0 N AY2 Z AH0 Z"])]
 
 -- Takes two words and compares their phonetic spellings and outputs True
 -- if they're are homophones of each other and false otherwise
+homophone :: String -> String -> Bool
 homophone x y = do 
     let dict = dictMap
     let phonX = Map.lookup (map toUpper x) dict
@@ -35,9 +44,11 @@ homophone x y = do
 matchAny :: Eq a => Maybe [a] -> Maybe [a] -> Bool
 matchAny Nothing _ = False
 matchAny _ Nothing = False
-matchAny (Just xs) (Just ys) = any ((flip elem) ys) xs
+matchAny (Just xs) (Just ys) = any (`elem` ys) xs
 
 -- Converts the phoneDict into a Map
 dictMap :: Map.Map String [String]
-dictMap = Map.fromList phoneDict
+dictMap = Map.fromList testList
 
+encodeArpabet :: String -> ARPABET
+encodeArpabet s = AA
