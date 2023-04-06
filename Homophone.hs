97f23255b959de24cc3e58a724d12aebd9ba06b0
Nurdayana Muhd Faisal <dam20@imperial.ac.uk>
2023-04-06 18:47:58 +0100

    Implemented fuzzy homophones. Threshold: 5

--- Homophone.hs
+++ Homophone.hs
@@ -7,44 +7,30 @@ import Data.IntMap (fromList)
 import Data.Char
 import System.IO.Unsafe
 import Data.Maybe
+import Data.Array
 
 import Format
-
-
-data ARPABET = AA | AE | AH | AO | AW | AX | AXR | AY | EH | ER | EY | -- VOWELS
-               IH | IX | IY | OW | OY | UH | UW | UX |                 -- VOWELS
-               B | CH | D | DH | DX | EL | EM | EN | F | G | HH | JH | -- CONSONENTS
-               K | L | M | N | NG | NX | P | Q | R | S | SH | T | TH | -- CONSONENTS
-               V | W | WH | Y | Z | ZH                                 -- CONSONENTS
-            deriving (Show, Ord, Eq, Read)
+import Arpabet
 
 data Accent = British | Cockney
             deriving (Eq)
 
 type Pronunciation = [ARPABET]
 
-testList :: [(String, [String])]
-testList = [("A",["AH0","EY1"]),
-    ("A'S",["EY1 Z"]),
-    ("A.",["EY1"]),
-    ("A.'S",["EY1 Z"]),
-    ("A.D.",["EY2 D IY1"]),
-    ("A.M.",["EY2 EH1 M"]),
-    ("THEIR", ["DH EH1 R"]), 
-    ("THERE", ["DH EH1 R"]), 
-    ("PATRON",["P EY1 T R AH0 N"]),
-    ("RED", ["R EH1 D"]),
-    ("REED",["R IY1 D"]),
-    ("READ",["R EH1 D","R IY1 D"]),
-    ("PATRONAGE",["P AE1 T R AH0 N IH0 JH","P EY1 T R AH0 N AH0 JH","P EY1 T R AH0 N IH0 JH"]),
-    ("PATRONIZES",["P EY1 T R AH0 N AY2 Z AH0 Z"])]
-
 -- TODO: changes out put type to be an int similarity level to support fuzzy homophones
 
 -- Takes two words and compares their phonetic spellings and outputs True
--- if they're are homophones of each other and false otherwise
+-- if they are pure homophones of each other and false otherwise
+pureHomophone :: [String] -> [String] -> Bool
+pureHomophone x y = matchAny (combine phonX) (combine phonY)
+    where
+        phonX = map (fromMaybe [] . lookupArpabet) x
+        phonY = map (fromMaybe [] . lookupArpabet) y
+
+-- Takes two words and compares their phonetic spellings and outputs True
+-- if their fuzzy score is below a certain threshold
 homophone :: [String] -> [String] -> Bool
-homophone x y = matchAny (combine phonX) (combine phonY)
+homophone x y = fuzzyScore (combine phonX) (combine phonY) < 5
     where
         phonX = map (fromMaybe [] . lookupArpabet) x
         phonY = map (fromMaybe [] . lookupArpabet) y
@@ -53,19 +39,21 @@ homophone x y = matchAny (combine phonX) (combine phonY)
 -- assumptions:
 --      * length of both words should be similar to be considered fuzzy
 --      * 100 is arbitrary high number to depict infinity score
+fuzzy :: [ARPABET] -> [ARPABET] -> Int
+fuzzy [] [] = 0
 fuzzy [] _ = 100
 fuzzy _ [] = 100
-fuzzy (x:xs) (y:ys) = score x y + fuzzy xs ys
-    where
-        -- TODO!!!!
-        score = undefined
+fuzzy (x:xs) (y:ys) = distMatrix ! (x, y) + fuzzy xs ys
+
+fuzzyScore :: [Pronunciation] -> [Pronunciation] -> Int
+fuzzyScore x y = minimum $ map (\z -> minimum $ map (fuzzy z) y) x
 
 -- provides a list of all combination of pronunciations that can exist for a list of words
 -- e.g. ["a","why"] which has pronunciations: [[["AH"],["EY"]],[["W","AY"],["HH","W","AY"]]]
 --      outputs [["AH","W","AY"],["AH","HH","W","AY"],["EY","W","AY"],["EY","HH","W","AY"]]
 combine :: [[Pronunciation]] -> [Pronunciation]
-combine [] = []
-combine [w] = w
+combine []       = []
+combine [w]      = w
 combine (w : ws) = combine' w (combine ws)
     where
         combine' :: [Pronunciation] -> [Pronunciation] -> [Pronunciation]
@@ -78,14 +66,14 @@ lookupArpabet s = Map.lookup (map toUpper s) dictMap
 
 -- outputs True if any elem from list xs matches any elem from list ys
 matchAny :: Eq a => [a] -> [a] -> Bool
-matchAny [] _ = False
+matchAny [] _  = False
 matchAny xs ys = any (`elem` ys) xs
 
 -- Converts the phoneDict into a HashMap for faster lookup
 dictMap :: Map.Map String [Pronunciation]
 dictMap = Map.fromList file
     where
-        file = map readDict $ lines getFile
+        file    = map readDict $ lines getFile
         getFile = unsafePerformIO $ do
             readFile "data/ArpabetDict.txt"
 
@@ -108,6 +96,7 @@ convertToAccent (a:as) act = a' ++ convertToAccent as act
 convertToBritish :: String -> [String]
 convertToBritish "R" = []
 convertToBritish "OW" = ["AX","UH"] 
-convertToBritish _ = ["rbit"]
+convertToBritish _ = ["init"]
 
 -- ============================================= CLUSTERING ============================================= --
+
